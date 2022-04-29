82891547a872f1e0dda8c735a1aca50d43cea863
Andrei Ladonin <dron201202@gmail.com>
2022-04-29 14:59:11 +0400

    29.04.2022

--- GlanApp/UserPageWindow.xaml.cs
+++ GlanApp/UserPageWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,68 @@ namespace GlanApp
         public UserPageWindow()
         {
             InitializeComponent();
+
+            btnCreateDocument.Click += BtnCreateDocument_Click;
+        }
+
+        private void BtnCreateDocument_Click(object sender, RoutedEventArgs e)
+        {
+            bool isWhiteSpace = !string.IsNullOrWhiteSpace(textBoxNumberDogovor.Text)
+                && !string.IsNullOrWhiteSpace(textBoxName.Text) && !string.IsNullOrWhiteSpace(textBoxPatronymic.Text)
+                && !string.IsNullOrWhiteSpace(textBoxSeriesPassport.Text) && !string.IsNullOrWhiteSpace(textBoxNumberPassport.Text)
+                && !string.IsNullOrWhiteSpace(textBoxLocationPassport.Text) && !string.IsNullOrWhiteSpace(textBoxCodePassport.Text)
+                && !string.IsNullOrWhiteSpace(textBoxAddressPassport.Text) && !string.IsNullOrWhiteSpace(textBoxPostEmployee.Text)
+                && !string.IsNullOrWhiteSpace(textBoxPostDepartament.Text) && !string.IsNullOrWhiteSpace(textBoxSalary.Text)
+                && !string.IsNullOrWhiteSpace(textBoxMounthCount.Text);
+
+            if (isWhiteSpace)
+            {
+                var data = new Dictionary<string, string> 
+                {
+                    { "<NUMBER_DOGOVOR>", textBoxNumberDogovor.Text },
+                    { "<DATE_DOGOVOR>",  DateTime.Now.ToString("dd.MM.yyyy")},
+                    { "<FIRST_NANE>", textBoxName.Text },
+                    { "<LAST_NAME>", textBoxLastName.Text},
+                    { "<PATRONYMIC>", textBoxPatronymic.Text },
+                    { "<SERIES_PASPORT>", textBoxSeriesPassport.Text},
+                    { "<NUMBER_PASPORT>", textBoxNumberPassport.Text },
+                    { "<POST_EMPLOYEE>", textBoxPostEmployee.Text },
+                    { "<POST_DEPARTAMENT>", textBoxPostDepartament.Text },
+                    { "<DATE_START_WORK>", textBoxDateDateStartWork.Text },
+                    { "<COUNT_MOUNTH>", textBoxMounthCount.Text },
+                    { "<SALARY>", textBoxSalary.Text },
+                    { "<FIRST_NANE_2>", textBoxName.Text },
+                    { "<LAST_NAME_2>", textBoxLastName.Text},
+                    { "<PATRONYMIC_2>", textBoxPatronymic.Text },
+                    { "<SERIES_PASPORT_2>", textBoxSeriesPassport.Text},
+                    { "<NUMBER_PASPORT_2>", textBoxNumberPassport.Text },
+                    { "<LOCATION_PASPORT>", textBoxLocationPassport.Text },
+                    { "<DATE_PASPORT>", textBoxDatePassport.Text },
+                    { "<CODE_DEPARTAMENT_PASPORT>", textBoxCodePassport.Text },
+                    { "<ADDRESS_REGISTR>", textBoxAddressPassport.Text},
+                    { "<DATE_DOGOVOR_2>",  DateTime.Now.ToString("dd.MM.yyyy")}
+                };
+
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Документ MS Word (*.doc)|*.doc";
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    var helper = new WordHelper("template_dogovr.doc", saveFileDialog.FileName);
+
+                    var isSave = helper.Process(data);
+                    if (isSave == true)
+                    {
+                        MessageBox.Show("Файл сохранен");
+                    }
+                    {
+                        MessageBox.Show("Ошибка!");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Проверьте введенность всех данных");
+            }
         }
     }
 }
---
+++ GlanApp/WordHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace GlanApp
+{
+    internal class WordHelper
+    {
+        private FileInfo _fileInfo;
+        private string _pathToSave;
+
+        public WordHelper(string fileName, string pathToSave)
+        {
+            if(File.Exists(fileName))
+            {
+                _fileInfo = new FileInfo(fileName);
+                _pathToSave = pathToSave;
+            } 
+            else
+            {
+                throw new ArgumentException("Файл не найден");
+            }
+        }
+
+        public bool Process(Dictionary<string, string> items)
+        {
+            Word.Application app = null;
+            try
+            {
+                app = new Word.Application();
+                Object file = _fileInfo.FullName;
+
+                Object missing = Type.Missing;
+
+                app.Documents.Open(file);
+
+                foreach (var item in items)
+                {
+                    Word.Find find = app.Selection.Find;
+                    find.Text = item.Key;
+                    find.Replacement.Text = item.Value;
+
+                    Object wrap = Word.WdFindWrap.wdFindContinue;
+                    Object replace = Word.WdReplace.wdReplaceAll;
+
+                    find.Execute(FindText: Type.Missing,
+                                MatchCase: false,
+                                MatchWholeWord: false,
+                                MatchWildcards: false,
+                                MatchSoundsLike: missing,
+                                MatchAllWordForms: false,
+                                Forward: true,
+                                Wrap: wrap,
+                                Format: false,
+                                ReplaceWith: missing, Replace: replace);
+                }
+
+                app.ActiveDocument.SaveAs2(_pathToSave);
+                app.ActiveDocument.Close();     
+
+                return true;
+            } catch (Exception ex)
+            {
+                return false;
+            } finally
+            {
+                if (app != null)
+                {
+                    app.Quit();
+                }
+            }
+        }
+    }
+}
